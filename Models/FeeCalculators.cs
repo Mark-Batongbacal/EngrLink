0d0ea714ecb724c95957eb117f147d6e28b71a8f
Mark Batongbacal <batongbacalmark@gmail.com>
2025-05-26 15:38:49 +0800

    fee calculator done

--- Models/FeeCalculators.cs
+++ Models/FeeCalculators.cs
@@ -6,12 +6,37 @@ using System.Threading.Tasks;
 
 namespace EngrLink.Models
 {
-    internal class FeeCalculators
+    public static class FeeCalculators
     {
-        public static int FeeCalculator(string year, string program, int units)
+        private static readonly Dictionary<(string year,string program), int> FeeMap = new()
+    {
+        { ("1st Year", "CPE"), 53000 },
+        { ("2nd Year", "CPE"), 55000 },
+        { ("3rd Year", "CPE"), 60000 },
+        { ("4th Year", "CPE"), 68000 },
+
+        { ("1st Year", "ARCHI"), 55000 },
+        { ("2nd Year", "ARCHI"), 59000 },
+        { ("3rd Year", "ARCHI"), 72000 },
+        { ("4th Year", "ARCHI"), 73000 },
+        { ("5th Year", "ARCHI"), 70000 },
+
+        { ("1st Year", "CE"), 53000 },
+        { ("2nd Year", "CE"), 57000 },
+        { ("3rd Year", "CE"), 60000 },
+        { ("4th Year", "CE"), 65000 },
+
+        { ("1st Year", "ECE"), 53000 },
+        { ("2nd Year", "ECE"), 55000 },
+        { ("3rd Year", "ECE"), 60000 },
+        { ("4th Year", "ECE"), 68000 }
+    };
+
+        public static int GetFee(string year, string program)
         {
-            int numyear = year[0];
-            return 2; //go mark
+            return FeeMap.TryGetValue((year, program), out var fee)
+                ? fee
+                : throw new ArgumentException("Fee not defined for given year and program");
         }
     }
 }
